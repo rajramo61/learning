ef7d0721829541310bf0a5fe0e487124c0b9a082
rajramo61 <rajesh.dwivedi@aol.com>
2015-07-29 19:16:55 -0700

    first update

---
+++ app/src/main/java/com/learningandroid/rajesh/moviesapp1/MovieImageAdapter.java
@@ -0,0 +1,37 @@
+package com.learningandroid.rajesh.moviesapp1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by Rajesh Kumar Dwivedi on 7/28/15.
+ */
+public class MovieImageAdapter extends ArrayAdapter<Integer> {
+
+    private final String LOG_MOVIE_ADAPTER = MovieImageAdapter.class.getSimpleName();
+
+    public MovieImageAdapter(Context context, List<Integer> resource) {
+        super(context, 0, resource);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Log.i(LOG_MOVIE_ADAPTER, "Inside getView");
+        int item = getItem(position);//It's the type of Adapter
+        //Log.i(LOG_MOVIE_ADAPTER, "Inside getView - after getItem");
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext())
+                            .inflate(R.layout.gridview_main, parent, false);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.grid_item_movie_imageview);
+        imageView.setImageResource(item);
+        return convertView;
+    }
+}
